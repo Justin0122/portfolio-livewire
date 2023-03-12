d5e81130aa95ee0ced9b9904ff616a2ee3decbf3
Justin <85618156+Justin0122@users.noreply.github.com>
2023-03-12 12:23:19 +0100

    Revert "Projects"

--- app/Http/Livewire/Counter.php
+++ app/Http/Livewire/Counter.php
@@ -9,6 +9,7 @@ class Counter extends Component
 {
     public int $count = 0;
 
+    //make a function that can use this <button wire:click="increment(5)">+5</button> in the view
     public function increment($amount = 1)
     {
         $this->count += $amount;
@@ -17,5 +18,10 @@ class Counter extends Component
     public function decrement($amount = 1)
     {
         $this->count -= $amount;
+    }
+
+    public function render()
+    {
+        return view('livewire.counter');
     }
 }
