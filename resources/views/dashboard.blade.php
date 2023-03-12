d0127096a4209f3aaef75f7d3fa4830a087e6bcc
Justin Jongstra <00300061@rijnijssel.nl>
2023-03-12 12:18:40 +0100

    counter

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,8 +1,8 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+        <x-header>
             {{ __('Dashboard') }}
-        </h2>
+        </x-header>
     </x-slot>
 
     <div class="py-12">
