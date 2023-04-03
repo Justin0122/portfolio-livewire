cef1954cc38a79831fd3cb30f8846408d89c9d83
Justin Jongstra <00300061@rijnijssel.nl>
2023-04-03 16:09:51 +0200

    added welcome page / minor changes

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,7 +6,9 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('home') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200"/>
+                        <div class="h-9 w-9">
+                            <x-application-logo/>
+                        </div>
                     </a>
                 </div>
                 <!-- Navigation Links -->
