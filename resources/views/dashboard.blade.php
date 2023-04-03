73bc090696d49a630347fc3cf37b94f8a2f0b5e3
Justin Jongstra <00300061@rijnijssel.nl>
2023-04-03 15:09:04 +0200

    added contact

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,6 +3,9 @@
         <x-header>
             {{ __('Dashboard') }}
         </x-header>
+        <x-anchor-link href="#projects">
+            {{ __('Projects') }}
+        </x-anchor-link>
         <x-anchor-link href="#about">
             {{ __('About') }}
         </x-anchor-link>
@@ -12,13 +15,18 @@
     </x-slot>
 
     <div class="py-12 text-gray-900 dark:text-gray-100">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 h-screen"
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 h-screen pt-24 mb-24"
              id="projects">
             <livewire:project-carousel/>
         </div>
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 h-screen pt-24"
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 h-screen pt-24 mb-24"
              id="about">
             @include('about')
+        </div>
+
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 pt-96"
+             id="contact">
+            @include('contact')
         </div>
     </div>
 </x-app-layout>
