5d349bdc0cb4cf69e0c1291d1d1d6d2fa386eca4
Justin Jongstra <00300061@rijnijssel.nl>
2023-04-12 10:21:04 +0200

    dropdown mobile fix

--- resources/views/livewire/project/project-show.blade.php
+++ resources/views/livewire/project/project-show.blade.php
@@ -1,53 +1,47 @@
 <div>
-    <div class="flex">
-        <div class=" content-center flex items-center justify-center">
-            <div class="px-4 py-12 sm:px-6">
-                @php
-                    $count = 1;
-                @endphp
-                <div class="my-slider">
+    @if (!empty($images))
+        <div class="flex">
+            <div class=" content-center flex items-center justify-center">
+                <div class="px-4 py-12 sm:px-6">
+                    @php
+                        $count = 1;
+                    @endphp
+                    <div class="my-slider">
 
-                    @if (empty($images))
-                        <div class="item">
-                            <div class="pl-4 pr-4">
-                                <img
-                                    src="/storage/default.png"
-                                    class="w-20 h-20 object-cover rounded-lg shadow-md">
+                        @foreach ($images as $image)
+                            <div class="item">
+                                @if (str_contains($image, 'jpg') || str_contains($image, 'png') || str_contains($image, 'jpeg'))
+                                    <div class="pl-4 pr-4">
+                                        <img
+                                            src="/storage/{{ $project->id }}/{{ $image }}"
+                                            class="h-full object-cover rounded-lg shadow-md w-full"
+                                            style="max-height: 500px"
+                                            alt=" {{ $project->title }} ">
+                                    </div>
+                                    @php
+                                        $count++;
+                                    @endphp
+                                @endif
                             </div>
-                        </div>
-                    @endif
-                    @foreach ($images as $image)
-                        <div class="item">
-                            @if (str_contains($image, 'jpg') || str_contains($image, 'png') || str_contains($image, 'jpeg'))
-                                <div class="pl-4 pr-4">
-                                    <img
-                                        src="/storage/{{ $project->id }}/{{ $image }}"
-                                        class="h-full object-cover rounded-lg shadow-md w-full"
-                                        alt=" {{ $project->title }} ">
-                                </div>
-                                @php
-                                    $count++;
-                                @endphp
-                            @endif
-                        </div>
-                    @endforeach
-                </div>
-                <div class="tns-controls flex justify-center items-center mt-4 space-x-2"
-                     aria-label="Carousel Navigation">
-                    <button
-                        class="tns-controls__item tns-controls__item--prev w-10 h-10 rounded-full bg-gray-100 dark:bg-gray-800 hover:bg-gray-200 dark:hover:bg-gray-700"
-                        aria-label="Previous slide"><
-                    </button>
-                    <button
-                        class="tns-controls__item tns-controls__item--next w-10 h-10 rounded-full bg-gray-100 dark:bg-gray-800 hover:bg-gray-200 dark:hover:bg-gray-700"
-                        aria-label=" Next slide">>
-                    </button>
+                        @endforeach
+                    </div>
+
+                    <div class="tns-controls flex justify-center items-center mt-4 space-x-2"
+                         aria-label="Carousel Navigation">
+                        <button
+                            class="tns-controls__item tns-controls__item--prev w-10 h-10 rounded-full bg-gray-100 dark:bg-gray-800 hover:bg-gray-200 dark:hover:bg-gray-700"
+                            aria-label="Previous slide"><
+                        </button>
+                        <button
+                            class="tns-controls__item tns-controls__item--next w-10 h-10 rounded-full bg-gray-100 dark:bg-gray-800 hover:bg-gray-200 dark:hover:bg-gray-700"
+                            aria-label=" Next slide">>
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-    <dl>
-    </dl>
+    @endif
+
     <div class="relative overflow-x-auto shadow-md sm:rounded-lg flex justify-center">
         <div class="mb-4 ml-8 mr-8 text-justify mt-8 break-words w-4/5">
             {!! html_entity_decode($project->description) !!}
